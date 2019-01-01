3ee952785bf98089073f0512deb372c63bba5c4f
Stephane Zermatten <szermatt@gmx.net>
2019-01-01 18:39:37 +0100

    scrollIntoViewIfNeeded that takes the navbar into accunt

---
+++ src/routes/_utils/scrollIntoView.js
@@ -0,0 +1,39 @@
+import {
+    getScrollContainer,
+    getOffsetHeight } from './scrollContainer'
+import { smoothScroll } from './smoothScroll'
+
+function getTopOverlay() {
+    return document.getElementById("main-nav").clientHeight
+}
+
+export function isInvisible(element) {
+    let rect = element.getBoundingClientRect()
+    return rect.top >= getOffsetHeight() ||
+        rect.bottom < 0 ||
+        rect.bottom < getTopOverlay()
+}
+
+export function scrollIntoViewIfNeeded(element) {
+    let rect = element.getBoundingClientRect()
+    let topOverlay = getTopOverlay()
+    let offsetHeight = getOffsetHeight()
+    let scrollY = 0
+    if (rect.top < topOverlay) {
+        scrollY = topOverlay
+    } else if (rect.bottom > offsetHeight) {
+        let height = rect.bottom - rect.top;
+        if ((offsetHeight - topOverlay) > height) {
+            scrollY = offsetHeight - height
+        } else {
+            // if element height is too great to fit,
+            // prefer showing the top of the element
+            scrollY = topOverlay
+        }
+    } else {
+        return;  // not needed
+    }
+    let scrollContainer = getScrollContainer()
+    let scrollTop = scrollContainer.scrollTop
+    smoothScroll(scrollContainer, scrollTop + rect.top - scrollY)
+}
