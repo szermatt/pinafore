743fb5daeed50d777e295dcc51827b61d66049ee
Stephane Zermatten <szermatt@gmx.net>
2019-01-03 23:37:21 +0100

    use global and modal as scope keys

--- src/routes/_utils/shortcuts.js
+++ src/routes/_utils/shortcuts.js
@@ -1,12 +1,22 @@
 
 const scopes = new Map()
 let inModal = false
+let currentScope = 'global'
+let scopeStack = []
 
-export function restrictToModalShortcuts(modal) {
-    inModal = modal
+export function pushShortcutScope(scope) {
+    scopeStack.push(currentScope)
+    currentScope = scope
 }
 
-export function acceptShortcutEvent(event, modal) {
+export function popShortcutScope(scope) {
+    if (scope != currentScope) {
+        return
+    }
+    currentScope = scopeStack.pop()
+}
+
+export function acceptShortcutEvent(event) {
     let target = event.target;
     if (target && (target.isContentEditable ||
                    target.tagName == 'INPUT' ||
@@ -15,14 +25,11 @@ export function acceptShortcutEvent(event, modal) {
         return false
     }
 
-    if ((inModal && !modal) || (!inModal && modal)) {
-        return false
-    }
     return true
 }
 
 export function onKeyDownInShortcutScope(scopeKey, event) {
-    let scope = scopes[scopeKey]
+    let scope = scopes.get(scopeKey)
     if (!scope) {
         return
     }
@@ -33,22 +40,35 @@ export function onKeyDownInShortcutScope(scopeKey, event) {
     }
 }
 
+function onKeyDown(event) {
+    if (!acceptShortcutEvent(event)) {
+        return
+    }
+    onKeyDownInShortcutScope(currentScope, event)
+}
+
 export function addToShortcutScope(key, component) {
-    let scope = scopes[key]
+    if (scopes.size == 0) {
+        window.addEventListener('keydown', onKeyDown)
+    }
+    let scope = scopes.get(key)
     if (!scope) {
         scope = new Array()
-        scopes[key] = scope
+        scopes.set(key, scope)
     }
     scope.push(component)
 }
 
 export function removeFromShortcutScope(key, component) {
-    let scope = scopes[key]
+    let scope = scopes.get(key)
     if (!scope) {
         return
     }
     scope.splice(scope.indexOf(component), 1)
     if (scope.length == 0) {
         scopes.delete(key)
+    }
+    if (scopes.size == 0) {
+        window.removeEventListener('keydown', onKeyDown)
     }
 }
