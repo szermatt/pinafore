fff2528deb461fdb232bc24761ab9209a97b66b6
Stephane Zermatten <szermatt@gmx.net>
2019-01-04 17:05:10 +0100

    Centralize prefix map handling in shortcut.js

--- src/routes/_utils/shortcuts.js
+++ src/routes/_utils/shortcuts.js
@@ -1,9 +1,5 @@
-
-const scopes = new Map()
-let inModal = false
 let currentScope = 'global'
 let scopeStack = []
-
 export function pushShortcutScope(scope) {
     scopeStack.push(currentScope)
     currentScope = scope
@@ -16,28 +12,74 @@ export function popShortcutScope(scope) {
     currentScope = scopeStack.pop()
 }
 
-export function acceptShortcutEvent(event) {
-    let target = event.target;
-    if (target && (target.isContentEditable ||
-                   target.tagName == 'INPUT' ||
-                   target.tagName == 'TEXTAREA' ||
-                   target.tagName == 'SELECT')) {
-        return false
+const scopeKeyMaps = new Map()
+let prefixMap = null
+let prefixMapScope = null
+export function addToShortcutScope(scopeKey, keys, component) {
+    if (scopeKeyMaps.size == 0) {
+        window.addEventListener('keydown', onKeyDown)
     }
+    let keyMap = scopeKeyMaps.get(scopeKey)
+    if (!keyMap) {
+        keyMap = new Map()
+        scopeKeyMaps.set(scopeKey, keyMap)
+    }
+    mapKeys(keyMap, keys, component)
+}
 
-    return true
+export function removeFromShortcutScope(scopeKey, keys, component) {
+    let keyMap = scopeKeyMaps.get(scopeKey)
+    if (!keyMap) {
+        return
+    }
+    unmapKeys(keyMap, keys, component)
+    if (keyMap.size == 0) {
+        scopeKeyMaps.delete(scopeKey)
+    }
+    if (scopeKeyMaps.size == 0) {
+        window.removeEventListener('keydown', onKeyDown)
+    }
+}
+
+const FALLBACK_KEY = '__fallback__'
+export function addShortcutFallback(scopeKey, component) {
+    addToShortcutScope(scopeKey, FALLBACK_KEY, component)
+}
+
+export function removeShortcutFallback(scopeKey, component) {
+    removeFromShortcutScope(scopeKey, FALLBACK_KEY, component)
 }
 
 export function onKeyDownInShortcutScope(scopeKey, event) {
-    let scope = scopes.get(scopeKey)
-    if (!scope) {
-        return
+    if (prefixMap) {
+        let handled = false
+        if (prefixMapScope == scopeKey) {
+            handled = handleEvent(scopeKey, prefixMap, event)
+        }
+        prefixMap = null
+        prefixMapScope = null
+        if (handled) {
+            return
+        }
     }
-    let len = scope.length
-    let i = -1
-    while (++i < len) {
-        scope[i].onKeyDown(event)
+    handleEvent(scopeKey, scopeKeyMaps.get(scopeKey), event)
+}
+
+function handleEvent(scopeKey, keyMap, event) {
+    if (!keyMap) {
+        return false
     }
+    let value = keyMap.get(event.key) || keyMap.get(FALLBACK_KEY)
+    if (!value) {
+        return false
+    }
+    if (Map.prototype.isPrototypeOf(value)) {
+        prefixMap = value
+        prefixMapScope = scopeKey
+    } else {
+        value.onKeyDown(event)
+    }
+    return true
 }
 
 function onKeyDown(event) {
@@ -47,28 +89,58 @@ function onKeyDown(event) {
     onKeyDownInShortcutScope(currentScope, event)
 }
 
-export function addToShortcutScope(key, component) {
-    if (scopes.size == 0) {
-        window.addEventListener('keydown', onKeyDown)
-    }
-    let scope = scopes.get(key)
-    if (!scope) {
-        scope = new Array()
-        scopes.set(key, scope)
-    }
-    scope.push(component)
+function mapKeys(keyMap, keys, component) {
+    keys.split('|').forEach(
+        (seq) => {
+            let seqArray = seq.split(' ')
+            let prefixLen = seqArray.length - 1
+            let currentMap = keyMap;
+            let i = -1
+            while (++i < prefixLen) {
+                let prefixMap = currentMap.get(seqArray[i])
+                if (!prefixMap) {
+                    prefixMap = new Map()
+                    currentMap.set(seqArray[i], prefixMap)
+                }
+                currentMap = prefixMap
+            }
+            currentMap.set(seqArray[prefixLen], component)
+        })
 }
 
-export function removeFromShortcutScope(key, component) {
-    let scope = scopes.get(key)
-    if (!scope) {
+function unmapKeys(keyMap, keys, component) {
+    keys.split('|').forEach(
+        (seq) => {
+            let seqArray = seq.split(' ')
+            let prefixLen = seqArray.length - 1
+            let currentMap = keyMap;
+            let i = -1
+            while (++i < prefixLen) {
+                let prefixMap = currentMap.get(seqArray[i])
+                if (!prefixMap) {
+                    return
+                }
+                currentMap = prefixMap
+            }
+            let lastKey = seqArray[prefixLen]
+            if (currentMap.get(lastKey) == component) {
+                currentMap.delete(lastKey)
+            }
+        })
+}
+
+function acceptShortcutEvent(event) {
+    if (event.metaKey || event.ctrlKey || event.shiftKey) {
         return
     }
-    scope.splice(scope.indexOf(component), 1)
-    if (scope.length == 0) {
-        scopes.delete(key)
+    
+    let target = event.target;
+    if (target && (target.isContentEditable ||
+                   target.tagName == 'INPUT' ||
+                   target.tagName == 'TEXTAREA' ||
+                   target.tagName == 'SELECT')) {
+        return false
     }
-    if (scopes.size == 0) {
-        window.removeEventListener('keydown', onKeyDown)
-    }
+    return true
 }
+
